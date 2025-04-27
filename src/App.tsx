c841f45e09c39fcdbc6d36d428d1611fcbac227d
Mateus Cabral <47371964+Cabraiz@users.noreply.github.com>
2025-04-27 01:43:52 -0300

    🚀 deploy rápido

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route, useParams } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useParams } from 'react-router-dom';
 
 // Importar componentes
 import Navbar from './components/Navbar/Navbar';
